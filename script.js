8c175f4a5e660066676eb50e3be237e6035f3b12
Helloimalex1223 <plattaalex@gmail.com>
2024-03-30 20:00:04 -0900

    Add input to get choice from the player.

--- script.js
+++ script.js
@@ -7,8 +7,20 @@ function chooseRandomHand()
 
 //The three choices in the game, mapped to values in an array
 
-let choices = ["Rock", "Paper", "Scissors"]
+let choices = ["rock", "paper", "scissors"]
 
 //The value output from the randomizer function is plugged into the array, returning a string with the random choice
 
 console.log(choices[chooseRandomHand()]);
+
+
+
+
+//Prompt the player for input in the form of a string. 
+//Ensure the entered values must be rock, paper, or scissors
+//Case sensitivity does not matter. Accept inputs in any case
+
+let playerChoice = prompt("Make your choice: Rock, Paper, or Scissors?").toLowerCase();
+
+
+console.log(playerChoice);
