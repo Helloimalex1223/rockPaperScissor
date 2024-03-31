6c2b77d7f44ddd06b898fa118982f205fb9f7c71
Helloimalex1223 <plattaalex@gmail.com>
2024-03-30 20:28:12 -0900

    add logic for determining the winner

--- script.js
+++ script.js
@@ -11,10 +11,7 @@ let choices = ["rock", "paper", "scissors"]
 
 //The value output from the randomizer function is plugged into the array, returning a string with the random choice
 
-console.log(choices[chooseRandomHand()]);
-
-
-
+let computerChoice = (choices[chooseRandomHand()]);
 
 //Prompt the player for input in the form of a string. 
 //Ensure the entered values must be rock, paper, or scissors
@@ -23,4 +20,29 @@ console.log(choices[chooseRandomHand()]);
 let playerChoice = prompt("Make your choice: Rock, Paper, or Scissors?").toLowerCase();
 
 
-console.log(playerChoice);
+//Create the logic for how each choice interacts with the other
+
+function determineWinner(playerchoice, computerChoice)
+{
+    //The following scenarios equal the player winning
+    if(playerChoice == "rock" && computerChoice == "scissors" ||
+       playerChoice == "paper" && computerChoice == "rock" ||
+       playerChoice == "scissors" && computerChoice == "paper")
+    {
+        console.log(`You won! You chose ${playerChoice} and the computer chose ${computerChoice}`)
+    }
+
+    //The following scenarios equal the computer winning
+    else if(playerChoice == "scissors" && computerChoice == "rock" ||
+       playerChoice == "rock" && computerChoice == "paper" ||
+       playerChoice == "paper" && computerChoice == "scissors")
+    {
+        console.log(`You lost! You chose ${playerChoice} and the computer chose ${computerChoice}`)
+    }
+
+    //The following scenarios result in a tie
+    else
+    {
+        console.log("It's a tie!!")
+    }
+}
